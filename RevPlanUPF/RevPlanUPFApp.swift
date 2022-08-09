662e3ecd3b217e2a1c7f78265dea383bcf6a66c4
Stéphane Sayeb <contact@tahitizoom.pf>
2022-08-08 22:19:34 -1000

    Initial Commit

---
+++ RevPlanUPF/RevPlanUPFApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevPlanUPFApp.swift
+//  RevPlanUPF
+//
+//  Created by Tahiti Zoom on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RevPlanUPFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
